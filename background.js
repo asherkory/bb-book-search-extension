4b2f20db64823f5bff361c6b87d70786a0665cea
Asher Kory <asher.kory@gmail.com>
2022-09-30 07:49:26 -0400

    Remove extras

---
+++ background.js
@@ -0,0 +1,9 @@
+console.log("background.js is running")
+
+// when a message is received from the content script, perform a search
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.searchQuery) {
+    const searchUrl = `https://www.bookbub.com/search?search=${request.searchQuery}`;
+    chrome.tabs.create({ url: searchUrl });
+  }
+});
