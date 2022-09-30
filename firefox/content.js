e1da265803e9c5f945946143101e9984e49347b7
Asher Kory <asher.kory@gmail.com>
2022-09-30 09:31:01 -0400

    Duplicate files for chrome and firefox

---
+++ firefox/content.js
@@ -0,0 +1,6 @@
+// when message is received from background worker, send text selection
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.method == "getSelection") {
+		sendResponse({ selectionText: document.getSelection().toString() });	
+	}
+});
